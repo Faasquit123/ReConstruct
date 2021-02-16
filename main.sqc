a5b6dfba8aa3cc9e964f59ab8c4e1305f5924684
Faasquit123 <75254299+Faasquit123@users.noreply.github.com>
2021-02-16 10:19:58 +0100

    Add files via upload

---
+++ main.sqc
@@ -0,0 +1,14 @@
+import GameEngine{
+    jsonname"options.json"
+    launchfile"srf.sqc"
+}
+time.add.async{
+    game.add.time
+    async.io.output{
+        player.output{
+            "/sv_cheats 1"
+            "/rules edit tickspeed 70 to 1"
+            "/sv_cheats 2"
+        }
+    }
+}
---
+++ src/Blocks2Id.py
@@ -0,0 +1,17 @@
+import GameEngine{
+    jsonname"options.json"
+    launchfile"srf.sqc"
+
+null = asyncio.faasquit.libcode
+}
+time.add.async{
+    game.add.time
+    async.io.output{
+        player.output{
+            "/sv_cheats 1"
+            "/rules edit tickspeed 70 to 1"
+            "/sv_cheats 2"
+            "select" null
+        }
+    }
+}
